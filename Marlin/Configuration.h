a6c2b062ebf0233f11c2e3c9be94dceb6871f96d
Nicholas Lee <csudcy@gmail.com>
2020-04-18 08:48:27 +0100

    Centre bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -732,7 +732,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 103.1 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 112 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1103,7 +1103,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define Z_MAX_POS 190
 
 /**
  * Software Endstops
@@ -1345,8 +1345,8 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-//#define MANUAL_X_HOME_POS 0
-//#define MANUAL_Y_HOME_POS 0
+#define MANUAL_X_HOME_POS -14
+#define MANUAL_Y_HOME_POS -6
 //#define MANUAL_Z_HOME_POS 0
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
